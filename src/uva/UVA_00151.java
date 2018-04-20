a780185f2ca72d004742146c088b01b8ef2ca2a8
Docente <Docente@FI-JABREU>
2018-04-20 17:13:40 -0400

    UVA_11057

---
+++ src/uva/UVA_00151.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uva;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Docente
+ */
+public class UVA_00151 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n;
+        
+        while((n=sc.nextInt())!=0){
+            boolean found=false;
+            int m=1;
+            while(!found){
+                int w = f(n-1, m)+1;
+                if(w==13){
+                    System.out.println(m);
+                    found=true;
+                }
+                m++;
+            }
+            
+        }
+    }
+    
+    public static int f(int n, int k){
+        if(n==1){
+            return 1;
+        }
+        else{
+            return ((f(n-1, k)+k-1)%n)+1;
+        }
+    }
+}
